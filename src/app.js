fa7b9888a43167669e44d6a1cc7d3223923d87f3
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-10-02 16:38:59 +0200

    remove promise babel mock

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import regeneratorRuntime from 'regenerator-runtime'; // (needed for async fns, see https://github.com/babel/babel/issues/9849)
 import mockedOpptjeningData from './mock/mockedOpptjeningData';
 import renderers from './ui/renderers';
 import './ui/components/aktivitetTabell.scss';
