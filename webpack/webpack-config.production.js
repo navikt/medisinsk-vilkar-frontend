d67e0d7d4f933c5aa573d92579503494d3a530dd
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-10-21 12:15:58 +0200

    Add excludeComments option to minimization config

--- webpack/webpack-config.production.js
+++ webpack/webpack-config.production.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 const commonWebpackConfig = require('./webpack.common.js');
 const pkg = require('./../package.json');
 
@@ -8,5 +9,9 @@ module.exports = merge(commonWebpackConfig, {
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, `../build/${pkg.version}`),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({ extractComments: false })],
     },
 });
