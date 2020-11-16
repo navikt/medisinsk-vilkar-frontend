c59d3c60f015341dc043edd2df76a268b4880277
Hallvard Andreas Stark <hallvard@scel.to>
2020-11-16 14:47:11 +0100

    fixed locale

--- src/util/formats.ts
+++ src/util/formats.ts
@@ -1,11 +1,10 @@
 import { Period } from '../types/Period';
 
 export const prettifyDate = (date: string) => {
-    return new Date(date).toLocaleDateString('no-NB');
+    return new Date(date).toLocaleDateString('nb-NO');
 };
 
-export const prettifyPeriod = ({ fom, tom }: Period) =>
-    `${prettifyDate(fom)} - ${prettifyDate(tom)}`;
+export const prettifyPeriod = ({ fom, tom }: Period) => `${prettifyDate(fom)} - ${prettifyDate(tom)}`;
 
 export const convertToInternationalPeriod = ({ fom, tom }: Period) => ({
     from: fom,
