f56899023b1265fa9632e8213ce402c149d1035d
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-11-27 13:50:59 +0100

    Move inline css into stylesheets in Vurderingsoversikt

--- src/ui/components/vurderingsoversikt/Vurderingsoversikt.tsx
+++ src/ui/components/vurderingsoversikt/Vurderingsoversikt.tsx
@@ -4,6 +4,7 @@ import Vurderingsvelger from '../vurderingsvelger/Vurderingsvelger';
 import Vurderingsdetaljer from '../vurderingsdetaljer/Vurderingsdetaljer';
 import { Undertittel } from 'nav-frontend-typografi';
 import { Knapp } from 'nav-frontend-knapper';
+import styles from './vurderingsoversikt.less';
 
 const finnValgtVurdering = (vurderinger, vurderingId) => {
     return vurderinger.find(({ id }) => vurderingId === id);
@@ -14,13 +15,21 @@ const Vurderingsoversikt = ({ vurderinger }) => {
     const [valgtVurdering, setValgtVurdering] = React.useState(finnValgtVurdering(vurderinger, vurdering) || null);
 
     return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ display: 'flex', flexWrap: 'wrap', maxWidth: '500px' }}>
+        <div className={styles.vurderingsoversikt}>
+            <div className={styles.vurderingsoversikt__navigationSection}>
                 <Undertittel>Alle perioder</Undertittel>
-                <Knapp type="standard" htmlType="button" mini={true} style={{ marginLeft: 'auto' }}>
+                <Knapp
+                    className={styles.vurderingsoversikt__navigationSection__nyVurderingKnapp}
+                    type="standard"
+                    htmlType="button"
+                    mini={true}
+                >
                     Opprett ny vurdering
                 </Knapp>
-                <div style={{ flexGrow: 1, marginTop: '1rem' }}>
+                <div
+                    className={styles.vurderingsoversikt__vurderingsvelgerContainer}
+                    style={{ flexGrow: 1, marginTop: '1rem' }}
+                >
                     <Vurderingsvelger
                         vurderinger={vurderinger}
                         onActiveVurderingChange={(nyvalgtVurdering) => {
