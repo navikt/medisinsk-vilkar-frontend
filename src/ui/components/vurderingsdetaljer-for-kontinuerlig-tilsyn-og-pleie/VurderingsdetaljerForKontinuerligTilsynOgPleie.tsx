6e470ff97d7e80851848510f25bf2fd7154d6b10
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-12-07 12:35:35 +0100

    Made detail view components for vilkårsvurderinger

--- src/ui/components/vurderingsdetaljer-for-kontinuerlig-tilsyn-og-pleie/VurderingsdetaljerForKontinuerligTilsynOgPleie.tsx
+++ src/ui/components/vurderingsdetaljer-for-kontinuerlig-tilsyn-og-pleie/VurderingsdetaljerForKontinuerligTilsynOgPleie.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Vurdering from '../../../types/Vurdering';
 import { prettifyPeriod } from '../../../util/formats';
-import styles from './vurderingsdetaljerForKontinuerligTilsynOgPleie.less';
 import DetailView from '../detail-view/DetailView';
+import LabelledContent from '../labelled-content/LabelledContent';
+import Vurderingsresultat from '../../../types/Vurderingsresultat';
+import Box, { Margin } from '../box/Box';
 
 interface VurderingsdetaljerForKontinuerligTilsynOgPleieProps {
     vurdering: Vurdering;
@@ -13,13 +15,33 @@ const VurderingsdetaljerForKontinuerligTilsynOgPleie = ({
 }: VurderingsdetaljerForKontinuerligTilsynOgPleieProps) => {
     return (
         <DetailView title="Vurdering av behov for kontinuerlig tilsyn og pleie">
-            <p className={styles.begrunnelse}>{begrunnelse}</p>
-            <p>{resultat}</p>
-            <ul>
-                {perioder.map((periode, i) => (
-                    <li key={`${i}`}>{prettifyPeriod(periode)}</li>
-                ))}
-            </ul>
+            <Box marginTop={Margin.medium}>
+                <LabelledContent label="Vurdering" content={<span>{begrunnelse}</span>} />
+            </Box>
+            <Box marginTop={Margin.large}>
+                <LabelledContent
+                    label="Utfall"
+                    content={
+                        <span>
+                            {resultat === Vurderingsresultat.INNVILGET
+                                ? 'Ja, det er behov for kontinuerlig tilsyn og pleie'
+                                : 'Nei, det er ikke behov for kontinuerlig tilsyn og pleie'}
+                        </span>
+                    }
+                />
+            </Box>
+            <Box marginTop={Margin.large}>
+                <LabelledContent
+                    label="Perioder vurdert"
+                    content={
+                        <ul style={{ margin: 0, listStyleType: 'none', padding: 0 }}>
+                            {perioder.map((periode, i) => (
+                                <li key={`${i}`}>{prettifyPeriod(periode)}</li>
+                            ))}
+                        </ul>
+                    }
+                />
+            </Box>
         </DetailView>
     );
 };
--- src/ui/components/vurderingsdetaljer-for-to-omsorgspersoner/VurderingsdetaljerForToOmsorgspersoner.tsx
+++ src/ui/components/vurderingsdetaljer-for-to-omsorgspersoner/VurderingsdetaljerForToOmsorgspersoner.tsx
@@ -3,6 +3,9 @@ import Vurdering from '../../../types/Vurdering';
 import { prettifyPeriod } from '../../../util/formats';
 import DetailView from '../detail-view/DetailView';
 import styles from './vurderingsdetaljerForToOmsorgspersoner.less';
+import LabelledContent from '../labelled-content/LabelledContent';
+import Box, { Margin } from '../box/Box';
+import Vurderingsresultat from '../../../types/Vurderingsresultat';
 
 interface VurderingsdetaljerForToOmsorgspersonerProps {
     vurdering: Vurdering;
@@ -13,13 +16,33 @@ const VurderingsdetaljerForToOmsorgspersoner = ({
 }: VurderingsdetaljerForToOmsorgspersonerProps) => {
     return (
         <DetailView title="Vurdering av to omsorgspersoner">
-            <p className={styles.begrunnelse}>{begrunnelse}</p>
-            <p>{resultat}</p>
-            <ul>
-                {perioder.map((periode, i) => (
-                    <li key={`${i}`}>{prettifyPeriod(periode)}</li>
-                ))}
-            </ul>
+            <Box marginTop={Margin.medium}>
+                <LabelledContent label="Vurdering" content={<span>{begrunnelse}</span>} />
+            </Box>
+            <Box marginTop={Margin.large}>
+                <LabelledContent
+                    label="Utfall"
+                    content={
+                        <span>
+                            {resultat === Vurderingsresultat.INNVILGET
+                                ? 'Ja, det er behov for to omsorgspersoner'
+                                : 'Nei, det er ikke behov for to omsorgspersoner'}
+                        </span>
+                    }
+                />
+            </Box>
+            <Box marginTop={Margin.large}>
+                <LabelledContent
+                    label="Perioder vurdert"
+                    content={
+                        <ul style={{ margin: 0, listStyleType: 'none', padding: 0 }}>
+                            {perioder.map((periode, i) => (
+                                <li key={`${i}`}>{prettifyPeriod(periode)}</li>
+                            ))}
+                        </ul>
+                    }
+                />
+            </Box>
         </DetailView>
     );
 };
