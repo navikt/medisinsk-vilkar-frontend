4402b746982c16a7b43c20e5f8a335d23895c5e6
Hallvard Andreas Stark <hallvard@scel.to>
2020-12-08 13:20:59 +0100

    spacing for scenario picker

--- src/ui/components/medisinsk-vilkår/Scenariovelger.tsx
+++ src/ui/components/medisinsk-vilkår/Scenariovelger.tsx
@@ -4,7 +4,9 @@ const id = 'scenario-picker';
 
 const Scenariovelger = ({ setScenario }) => (
     <div>
-        <label htmlFor={id}>Velg scenario</label>
+        <label htmlFor={id} style={{ marginRight: '0.5rem' }}>
+            Velg scenario
+        </label>
         <select id={id} onChange={(event) => setScenario(event.target.value)}>
             <option value={0}>Mor som søker</option>
             <option value={1}>Far som søker</option>
