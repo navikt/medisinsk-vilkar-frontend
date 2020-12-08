9bd4056674f539ae2ccfb1c86de45ac8ce025132
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-12-08 09:42:08 +0100

    Fix missing limitation props etc in PeriodpickerList

--- src/ui/form/wrappers/PeriodpickerList.tsx
+++ src/ui/form/wrappers/PeriodpickerList.tsx
@@ -71,6 +71,7 @@ const PeriodpickerList = ({
                                         return (
                                             <>
                                                 <PureDatepicker
+                                                    {...fromDatepickerProps}
                                                     label={fromDatepickerProps.label}
                                                     ariaLabel={fromDatepickerProps.ariaLabel}
                                                     value={value?.fom || ''}
@@ -80,6 +81,7 @@ const PeriodpickerList = ({
                                                 />
                                                 <div style={{ display: 'flex', marginLeft: '1rem' }}>
                                                     <PureDatepicker
+                                                        {...toDatepickerProps}
                                                         label={toDatepickerProps.label}
                                                         ariaLabel={toDatepickerProps.ariaLabel}
                                                         value={value?.tom || ''}
