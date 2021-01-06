e936c45425c8adffe585696e32ff4d3b4e108fec
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-01-06 08:50:04 +0100

    Parter label (#38)
    * adjusted vurdering to match actual api
    
    * check if overlapp, add label for overlapp
    
    * added tooltip component
    showing tooltip when hover over icon
    added new icons
    
    * css renaming
    removed unused code
    
    * spacing adjustments

---
+++ src/ui/components/icon-with-tooltip/IconWithTooltip.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import styles from './iconWithTooltip.less';
+
+interface IconWithTooltipProps {
+    renderIcon: () => React.ReactNode;
+    tooltipText: string;
+}
+
+const IconWithTooltip = ({ renderIcon, tooltipText }: IconWithTooltipProps): JSX.Element => (
+    <div className={styles.iconWithTooltip}>
+        {renderIcon()}
+        <div className={styles.iconWithTooltip__tooltipText}>{tooltipText}</div>
+    </div>
+);
+
+export default IconWithTooltip;
--- src/ui/components/vurderingsperiode/VurderingsperiodeElement.tsx
+++ src/ui/components/vurderingsperiode/VurderingsperiodeElement.tsx
@@ -4,12 +4,18 @@ import { Period } from '../../../types/Period';
 import Vurderingsresultat from '../../../types/Vurderingsresultat';
 import { prettifyPeriod } from '../../../util/formats';
 import GreenCheckIconFilled from '../icons/GreenCheckIconFilled';
+import OnePersonIconBlue from '../icons/OnePersonIconBlue';
+import OnePersonOutline from '../icons/OnePersonOutline';
 import RedCrossIconFilled from '../icons/RedCrossIconFilled';
+import TwoPersonsWithOneHighlightedIconBlue from '../icons/TwoPersonsWithOneHighlightedIconBlue';
+import IconWithTooltip from '../icon-with-tooltip/IconWithTooltip';
 import styles from './vurderingsperiodeElement.less';
 
 interface VurderingsperiodeElementProps {
     periode: Period;
     resultat: Vurderingsresultat;
+    gjelderForAnnenPart?: boolean;
+    gjelderForSøker?: boolean;
     etikett?: string;
 }
 
@@ -31,13 +37,35 @@ const renderResultatText = (resultat: Vurderingsresultat) => {
     }
 };
 
-const VurderingsperiodeElement = ({ periode, resultat, etikett }: VurderingsperiodeElementProps) => {
+const VurderingsperiodeElement = ({
+    periode,
+    resultat,
+    etikett,
+    gjelderForAnnenPart,
+    gjelderForSøker,
+}: VurderingsperiodeElementProps): JSX.Element => {
+    const parterLabel = () => {
+        if (gjelderForAnnenPart && gjelderForSøker) {
+            return (
+                <IconWithTooltip
+                    renderIcon={() => <TwoPersonsWithOneHighlightedIconBlue />}
+                    tooltipText="Søker og annen part"
+                />
+            );
+        }
+        if (gjelderForAnnenPart) {
+            return <IconWithTooltip renderIcon={() => <OnePersonOutline />} tooltipText="Annen part" />;
+        }
+        return <IconWithTooltip renderIcon={() => <OnePersonIconBlue />} tooltipText="Søker" />;
+    };
+
     return (
         <div className={styles.vurderingsperiodeElement}>
             {renderIcon(resultat)}
             <div className={styles.vurderingsperiodeElement__texts}>
                 <p className={styles.vurderingsperiodeElement__texts__period}>{prettifyPeriod(periode)}</p>
-                {renderResultatText(resultat)}
+                <div className={styles.vurderingsperiodeElement__texts__parterIcon}>{parterLabel()}</div>
+                <p className={styles.vurderingsperiodeElement__texts__parter}>{renderResultatText(resultat)}</p>
             </div>
             {etikett && (
                 <div className={styles.vurderingsperiodeElement__etikett}>
