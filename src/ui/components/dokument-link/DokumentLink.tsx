8e6fef095d40c2ca2cd279ea71986c715b577f80
Hallvard Andreas Stark <hallvard@scel.to>
2021-01-07 11:06:25 +0100

    added icon with tooltip for documents

--- src/ui/components/dokument-link/DokumentLink.tsx
+++ src/ui/components/dokument-link/DokumentLink.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Lenke from 'nav-frontend-lenker';
 import Dokument from '../../../types/Dokument';
 import { prettifyDate } from '../../../util/formats';
+import IconWithTooltip from '../icon-with-tooltip/IconWithTooltip';
+import OnePersonOutline from '../icons/OnePersonOutline';
+import styles from './dokumentLink.less';
 
 interface DokumentLinkProps {
     dokument: Dokument;
+    etikett?: string;
 }
 
-const DokumentLink = ({ dokument }: DokumentLinkProps) => {
+const DokumentLink = ({ dokument, etikett }: DokumentLinkProps) => {
     const { type, datert, location } = dokument;
     return (
         <Lenke
@@ -18,6 +22,15 @@ const DokumentLink = ({ dokument }: DokumentLinkProps) => {
             }}
         >
             {type} ({prettifyDate(datert.toDateString())})
+            <div className={styles.dokumentLink__etikett}>
+                {etikett && (
+                    <IconWithTooltip
+                        renderIcon={() => <OnePersonOutline />}
+                        tooltipText={etikett}
+                        tooltipDirectionRight
+                    />
+                )}
+            </div>
         </Lenke>
     );
 };
