0c5f1f3fe46f28be8e3a50505eeb66518538a236
Erlend Vige <erlend.edvardsen.vige@nav.no>
2021-03-01 11:21:25 +0100

    Remove content without data in Infostripe

--- src/ui/components/infostripe/Infostripe.tsx
+++ src/ui/components/infostripe/Infostripe.tsx
@@ -1,4 +1,3 @@
-import { Element } from 'nav-frontend-typografi';
 import * as React from 'react';
 import ChildIcon from '../icons/ChildIcon';
 import styles from './infostripe.less';
@@ -10,12 +9,6 @@ const Infostripe = () => (
         </div>
         <div className={styles.infostripe__textContainer}>
             <p className={styles.infostripe__text}>Sykdomsvurderingen gjøres på barnet og er felles for alle parter.</p>
-            <p className={styles.infostripe__text}>
-                Barnets alder: <Element tag="span">5 år</Element>
-            </p>
-            <p className={styles.infostripe__text}>
-                Diagnose: <Element tag="span">Kode mangler</Element>
-            </p>
         </div>
     </div>
 );
