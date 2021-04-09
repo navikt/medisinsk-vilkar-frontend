c33c1e5cc9be3cd618304908ec3ec51b5a5300c4
Hallvard Andreas Stark <hallvard@scel.to>
2021-04-09 10:49:04 +0200

    sjekker om ny vurdering har perioder

--- src/ui/components/detail-view-vurdering/DetailViewVurdering.tsx
+++ src/ui/components/detail-view-vurdering/DetailViewVurdering.tsx
@@ -3,6 +3,7 @@ import DetailView, { DetailViewProps } from '../detail-view/DetailView';
 import { Period } from '../../../types/Period';
 import PeriodList from '../period-list/PeriodList';
 import styles from './detailViewVurdering.less';
+import { isValidPeriod } from '../../../util/dateUtils';
 
 interface DetailViewVurderingProps extends DetailViewProps {
     perioder: Period[];
@@ -10,9 +11,10 @@ interface DetailViewVurderingProps extends DetailViewProps {
 
 const DetailViewVurdering = (props: DetailViewVurderingProps) => {
     const { children, perioder, ...detailViewProps } = props;
+    const harPerioder = perioder.length > 0 && isValidPeriod(perioder[0]);
     return (
         <DetailView {...detailViewProps} className={styles.detailViewVurdering}>
-            <PeriodList periods={perioder} className={styles.detailViewVurdering__periodList} />
+            {harPerioder && <PeriodList periods={perioder} className={styles.detailViewVurdering__periodList} />}
             <hr className={styles.detailViewVurdering__hr} />
             {children}
         </DetailView>
