b7a0bd469de019984d09b66dea3106633c889e43
Hallvard Andreas Stark <hallvard@scel.to>
2021-08-17 20:12:18 +0200

    henter ut feilmelding fra backend og viser til bruker

--- src/ui/components/strukturer-dokument-controller/StrukturerDokumentController.tsx
+++ src/ui/components/strukturer-dokument-controller/StrukturerDokumentController.tsx
@@ -1,11 +1,13 @@
 import { post } from '@navikt/k9-http-utils';
-import React, { useMemo } from 'react';
+import { Box, Margin } from '@navikt/k9-react-components';
 import axios from 'axios';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
+import React, { useMemo } from 'react';
 import Dokument from '../../../types/Dokument';
-import StrukturerDokumentForm from '../strukturer-dokument-form/StrukturerDokumentForm';
 import Link from '../../../types/Link';
-import ContainerContext from '../../context/ContainerContext';
 import scrollUp from '../../../util/viewUtils';
+import ContainerContext from '../../context/ContainerContext';
+import StrukturerDokumentForm from '../strukturer-dokument-form/StrukturerDokumentForm';
 
 interface StrukturerDokumentControllerProps {
     strukturerDokumentLink: Link;
@@ -25,6 +27,7 @@ const StrukturerDokumentController = ({
     const { httpErrorHandler } = React.useContext(ContainerContext);
     const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false);
     const httpCanceler = useMemo(() => axios.CancelToken.source(), []);
+    const [submitDocumentError, setSubmitDocumentError] = React.useState(null);
 
     React.useEffect(
         () => () => {
@@ -35,6 +38,7 @@ const StrukturerDokumentController = ({
 
     const strukturerDokument = (strukturertDokument: Dokument) => {
         setIsSubmitting(true);
+        setSubmitDocumentError(null);
         post(href, { ...requestPayload, ...strukturertDokument }, httpErrorHandler, {
             cancelToken: httpCanceler.token,
         }).then(
@@ -43,21 +47,42 @@ const StrukturerDokumentController = ({
                 onDokumentStrukturert();
                 scrollUp();
             },
-            () => {
+            (error) => {
+                setSubmitDocumentError(error);
                 setIsSubmitting(false);
                 scrollUp();
             }
         );
     };
 
+    const hasError = !!submitDocumentError;
+    const getErrorMessage = () => {
+        if (submitDocumentError?.response?.data?.feilmelding) {
+            return `${submitDocumentError.response.data.feilmelding}`;
+        }
+        return null;
+    };
+
     return (
-        <StrukturerDokumentForm
-            dokument={dokument}
-            onSubmit={strukturerDokument}
-            editMode={editMode}
-            isSubmitting={isSubmitting}
-            strukturerteDokumenter={strukturerteDokumenter}
-        />
+        <>
+            {hasError && (
+                <Box marginBottom={Margin.medium}>
+                    <AlertStripeFeil>{getErrorMessage()}</AlertStripeFeil>
+                </Box>
+            )}
+            <StrukturerDokumentForm
+                dokument={dokument}
+                onSubmit={strukturerDokument}
+                editMode={editMode}
+                isSubmitting={isSubmitting}
+                strukturerteDokumenter={strukturerteDokumenter}
+            />
+            {hasError && (
+                <Box marginTop={Margin.medium}>
+                    <AlertStripeFeil>{getErrorMessage()}</AlertStripeFeil>
+                </Box>
+            )}
+        </>
     );
 };
 
