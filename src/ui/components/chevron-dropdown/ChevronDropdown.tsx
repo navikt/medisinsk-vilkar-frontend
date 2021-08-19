dc4b711a7a913aee1f055a7b4fc61e56b974a063
vebnor <vebjorn.nordby@hotmail.com>
2021-08-19 14:43:49 +0200

    ChevronDropdown viser dropdown

--- src/ui/components/chevron-dropdown/ChevronDropdown.tsx
+++ src/ui/components/chevron-dropdown/ChevronDropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Element } from 'nav-frontend-typografi';
 import Chevron from 'nav-frontend-chevron';
+import { Checkbox } from 'nav-frontend-skjema';
 import classNames from 'classnames';
 import styles from './chevronDropdown.less';
 
@@ -13,12 +14,38 @@ function ChevronDropdown({ text, className }: ChevronDropdownProps): JSX.Element
     const [aktiv, settAktiv] = useState(false);
     const type = aktiv ? 'opp' : 'ned';
     return (
-        <span className={classNames(styles.chevronDropdown, className)}>
-            <button type="button" onClick={() => settAktiv(!aktiv)}>
-                <Element className={styles.chevronDropdown__text}>{text}</Element>
-                <Chevron type={type} />
-            </button>
-        </span>
+        <>
+            <span className={classNames(styles.chevronDropdown, className, aktiv && styles.chevronDropdown__hidden)}>
+                <button
+                    type="button"
+                    className={styles.chevronDropdown__toggleButton}
+                    onClick={() => settAktiv(!aktiv)}
+                >
+                    <Element className={styles.chevronDropdown__toggleButton__text}>{text}</Element>
+                    <Chevron type={type} />
+                </button>
+            </span>
+            {aktiv && (
+                <div className={classNames(styles.chevronDropdown__dropdown, className)}>
+                    <span className={classNames(styles.chevronDropdown)}>
+                        <button
+                            type="button"
+                            className={styles.chevronDropdown__toggleButton}
+                            onClick={() => settAktiv(!aktiv)}
+                        >
+                            <Element className={styles.chevronDropdown__toggleButton__text}>{text}</Element>
+                            <Chevron type={type} />
+                        </button>
+                        <div className={styles.chevronDropdown__dropdown__checkbox}>
+                            <Checkbox label="Ikke klassifisert" />
+                            <Checkbox label="Legeerklæring sykhus/spesialist." />
+                            <Checkbox label="Andre med. oppl." />
+                            <Checkbox label="Ikke med. oppl." />
+                        </div>
+                    </span>
+                </div>
+            )}
+        </>
     );
 }
 
