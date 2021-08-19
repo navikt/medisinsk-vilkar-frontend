89e53f23f8b6dbff9111b89c8da43a5058a9835d
vebnor <vebjorn.nordby@hotmail.com>
2021-08-19 16:07:43 +0200

    fungerende filtrering av dokumenter

--- src/ui/components/chevron-dropdown/ChevronDropdown.tsx
+++ src/ui/components/chevron-dropdown/ChevronDropdown.tsx
@@ -3,16 +3,25 @@ import { Element } from 'nav-frontend-typografi';
 import Chevron from 'nav-frontend-chevron';
 import { Checkbox } from 'nav-frontend-skjema';
 import classNames from 'classnames';
+import { Dokumenttype, dokumentLabel } from '../../../types/Dokument';
 import styles from './chevronDropdown.less';
 
 interface ChevronDropdownProps {
     text: string;
     className: string;
+    dokumenttypeFilter: Array<Dokumenttype>;
+    setDokumenttypeFilter: (value: Array<Dokumenttype>) => void;
 }
 
-function ChevronDropdown({ text, className }: ChevronDropdownProps): JSX.Element {
+function ChevronDropdown({
+    text,
+    className,
+    dokumenttypeFilter,
+    setDokumenttypeFilter,
+}: ChevronDropdownProps): JSX.Element {
     const [aktiv, settAktiv] = useState(false);
-    const type = aktiv ? 'opp' : 'ned';
+    const chevronType = aktiv ? 'opp' : 'ned';
+    const dokumenttypeListe = [...Object.values(Dokumenttype)];
     return (
         <>
             <span className={classNames(styles.chevronDropdown, className, aktiv && styles.chevronDropdown__hidden)}>
@@ -22,7 +31,7 @@ function ChevronDropdown({ text, className }: ChevronDropdownProps): JSX.Element
                     onClick={() => settAktiv(!aktiv)}
                 >
                     <Element className={styles.chevronDropdown__toggleButton__text}>{text}</Element>
-                    <Chevron type={type} />
+                    <Chevron type={chevronType} />
                 </button>
             </span>
             {aktiv && (
@@ -34,13 +43,21 @@ function ChevronDropdown({ text, className }: ChevronDropdownProps): JSX.Element
                             onClick={() => settAktiv(!aktiv)}
                         >
                             <Element className={styles.chevronDropdown__toggleButton__text}>{text}</Element>
-                            <Chevron type={type} />
+                            <Chevron type={chevronType} />
                         </button>
                         <div className={styles.chevronDropdown__dropdown__checkbox}>
-                            <Checkbox label="Ikke klassifisert" />
-                            <Checkbox label="Legeerklæring sykhus/spesialist." />
-                            <Checkbox label="Andre med. oppl." />
-                            <Checkbox label="Ikke med. oppl." />
+                            {dokumenttypeListe.map((type) => (
+                                <Checkbox
+                                    key={type}
+                                    label={dokumentLabel[type]}
+                                    checked={dokumenttypeFilter.includes(type)}
+                                    onChange={() =>
+                                        dokumenttypeFilter.includes(type)
+                                            ? setDokumenttypeFilter(dokumenttypeFilter.filter((v) => v !== type))
+                                            : setDokumenttypeFilter(dokumenttypeFilter.concat([type]))
+                                    }
+                                />
+                            ))}
                         </div>
                     </span>
                 </div>
