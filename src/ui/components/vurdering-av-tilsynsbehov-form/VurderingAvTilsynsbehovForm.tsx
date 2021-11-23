c086c5162a6d4684ea5f300d1858abb16850fdd6
vebnor <vebjorn.nordby@nav.no>
2021-11-23 15:20:36 +0100

    Revert "Merge pull request #135 from navikt/vise-navn-paa-saksbehandler-som-har-gjort-vurdering"
    This reverts commit decc822fbbec458c7c31b6a48a3ccf1427d387cc, reversing
    changes made to 5c1718fbaa6b21b03dac375a8ad1692c9c7fb08c.

--- src/ui/components/vurdering-av-tilsynsbehov-form/VurderingAvTilsynsbehovForm.tsx
+++ src/ui/components/vurdering-av-tilsynsbehov-form/VurderingAvTilsynsbehovForm.tsx
@@ -194,7 +194,7 @@ const VurderingAvTilsynsbehovForm = ({
                                 <CheckboxGroup
                                     question="Hvilke dokumenter er brukt i vurderingen av tilsyn og pleie?"
                                     name={FieldName.DOKUMENTER}
-                                    checkboxes={getDokumenterSomSkalVises().map((dokument) => ({
+                                    checkboxes={getDokumenterSomSkalVises().map((dokument, index) => ({
                                         value: dokument.id,
                                         label: (
                                             <DokumentLink
