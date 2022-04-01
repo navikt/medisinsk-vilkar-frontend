a1fbc50b78b27d394575fd1726a5535a49176857
Hallvard Andreas Stark <hallvard@scel.to>
2022-04-01 16:13:27 +0200

    bruker nye pakker fra k9-frontend-modules

--- src/ui/components/vurdering-av-tilsynsbehov-form/VurderingAvTilsynsbehovForm.tsx
+++ src/ui/components/vurdering-av-tilsynsbehov-form/VurderingAvTilsynsbehovForm.tsx
@@ -123,7 +123,7 @@ const VurderingAvTilsynsbehovForm = ({
                 }
                 return period;
             })
-            .flatMap((p) => p.asListOfDays());
+            .flatMap((p) => new Period(p.fom, p.tom).asListOfDays());
         return dagerSomSkalVurderes.every((dagSomSkalVurderes) => dagerSomBlirVurdert.indexOf(dagSomSkalVurderes) > -1);
     }, [resterendeVurderingsperioder, perioderSomBlirVurdert]);
 
